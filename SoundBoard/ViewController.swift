1b7603835119ce9542b974419b23e2e77b139c59
munhoven <munhoven@gmail.com>
2017-05-26 19:28:56 +0200

    Main VC set up (no delete yet)

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import AVFoundation
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // link of Table View in Main VC
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Array of sounds in table view
+    var sounds : [Sound] = []
+    var audioPlayer : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            sounds = try context.fetch(Sound.fetchRequest())
+        } catch {}
+        
+        tableView.reloadData()
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let sound = sounds[indexPath.row]
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: sound.audio! as Data)
+            audioPlayer?.play()
+        } catch {}
 
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return sounds.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let sound = sounds[indexPath.row]
+        cell.textLabel?.text = sound.name
+        return cell
+        
+    }
+    
 }
+
 
